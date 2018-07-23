36b3e8035a04e0790000c05b62878e9c8bc7fc18
Arda Tümay <arda.tumay@tedu.edu.tr>
2018-07-23 17:36:59 +0300

    router print for debug is removed

--- src/components/Main.js
+++ src/components/Main.js
@@ -107,7 +107,6 @@ class Main extends Component {
         <View style={styles.container}>
           <View style={styles.subContainer}>{this.showTab()}</View>
         </View>
-        <Text> {this.state.activeTab}</Text>
         <BottomNavigation
           //activeTab={this.state.activeTab}
           onTabPress={newTab => {
