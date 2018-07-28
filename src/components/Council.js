2a6ccc37deef40d2ed4302156980572798ec2648
Arda Tümay <arda.tumay@tedu.edu.tr>
2018-07-28 14:38:45 +0300

    Some improvements for tabnav

--- src/components/Council.js
+++ src/components/Council.js
@@ -56,7 +56,7 @@ class Council extends Component {
 
     if (winHeight < 736) {
         console.log("device height less than 736");
-        this.setState({ MAX_HEIGHT: winHeight * 0.175 }); //17.5%
+        this.setState({ MAX_HEIGHT: winHeight * 0.21 }); //17.5%
     } else if (winHeight >= 736) {
         console.log("device height greater than 736");
         this.setState({ MAX_HEIGHT: winHeight * 0.18 }); //18%
@@ -87,35 +87,10 @@ class Council extends Component {
                                />}
             overlayColor="#144d8c"
             maxOverlayOpacity={1}
-            scrollEnabled={false}
-            
-        >
-        <View height={this.state.scrollHeight}>
-        <ImageBackground source={require("./img/background/BACKGROUND.png")} style={styles.mainBackGround}>
-        
-            <View style={styles.container}>
-                <View style={styles.questionContainer}>
-                    <View style={styles.question}>
-                    <Text style={styles.text}>
-                        Where do you want to go for the party?
-                    </Text>
-                    </View>
-                    <View style={styles.answers} >
-                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor1 }]} onPress={() => { this.setState({ buttonBackgroundColor1: "rgb(22,103,163)", clicked1: true }); if (this.state.clicked1) { this.setState({ buttonBackgroundColor1: "transparent", clicked1: false }); console.log(this.state.buttonBackgroundColor1); } }}>
-                        <Text> 6:45</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor2 }]} onPress={() => { this.setState({ buttonBackgroundColor2: "rgb(22,103,163)", clicked2: true }); if (this.state.clicked2) { this.setState({ buttonBackgroundColor2: "transparent", clicked2: false }); console.log(this.state.buttonBackgroundColor2); } }}>
-                        <Text> Bomonti</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor3 }]} onPress={() => { this.setState({ buttonBackgroundColor3: "rgb(22,103,163)", clicked3: true }); if (this.state.clicked3) { this.setState({ buttonBackgroundColor3: "transparent", clicked3: false }); console.log(this.state.buttonBackgroundColor3); } }}>
-                        <Text> Lux the mix</Text>
-                    </TouchableOpacity>
-                    </View>
-                </View>
-            </View>
-        </ImageBackground>
-        </View>
-        <TabNavigator tabBarStyle={styles.tabNav} >
+            bounces={false}
+            renderTouchableFixedForeground={() => { return ( 
+
+            <TabNavigator tabBarStyle={styles.tabNav} sceneStyle={{ height: 0 }} >
                 <TabNavigator.Item
                     selected={this.state.selectedTab === 'News'}
                     title="News"
@@ -133,9 +108,59 @@ class Council extends Component {
                     onPress={() => this.setState({ selectedTab: 'Votings' })}
                     titleStyle={styles.tabNavTitle}
                 >
-                    <Text> tab2 </Text>
+                <Text> tab2 </Text>
                 </TabNavigator.Item>
-        </TabNavigator>
+            </TabNavigator>
+
+            ); }}
+            
+        >
+        <View height={700}>
+        <ImageBackground source={require("./img/background/BACKGROUND.png")} style={styles.mainBackGround}>
+        <View style={styles.container}>
+                <View style={styles.questionContainer}>
+                    <View style={styles.question}>
+                    <Text style={styles.text}>
+                        Where do you want to go for the party?
+                    </Text>
+                    </View>
+                    <View style={styles.answers} >
+                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor1 }]} onPress={() => { this.setState({ buttonBackgroundColor1: "rgb(22,103,163)", clicked1: true }); if (this.state.clicked1) { this.setState({ buttonBackgroundColor1: "transparent", clicked1: false }); console.log(this.state.clicked1); } }}>
+                        <Text> 6:45</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor2 }]} onPress={() => { this.setState({ buttonBackgroundColor2: "rgb(22,103,163)", clicked2: true }); if (this.state.clicked2) { this.setState({ buttonBackgroundColor2: "transparent", clicked2: false }); console.log(this.state.clicked2); } }}>
+                        <Text> Bomonti</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor3 }]} onPress={() => { this.setState({ buttonBackgroundColor3: "rgb(22,103,163)", clicked3: true }); if (this.state.clicked3) { this.setState({ buttonBackgroundColor3: "transparent", clicked3: false }); console.log(this.state.clicked3); } }}>
+                        <Text> Lux the mix</Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
+                <View style={styles.questionContainer}>
+                    <View style={styles.question}>
+                    <Text style={styles.text}>
+                        Which date do you want to have a party?
+                    </Text>
+                    </View>
+                    <View style={styles.answers} >
+                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor1 }]} onPress={() => { this.setState({ buttonBackgroundColor1: "rgb(22,103,163)", clicked1: true }); if (this.state.clicked1) { this.setState({ buttonBackgroundColor1: "transparent", clicked1: false }); console.log(this.state.clicked1); } }}>
+                        <Text> 20-05-2019 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor2 }]} onPress={() => { this.setState({ buttonBackgroundColor2: "rgb(22,103,163)", clicked2: true }); if (this.state.clicked2) { this.setState({ buttonBackgroundColor2: "transparent", clicked2: false }); console.log(this.state.clicked2); } }}>
+                        <Text> 15-07-2019 </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={[styles.answerButton, { backgroundColor: this.state.buttonBackgroundColor3 }]} onPress={() => { this.setState({ buttonBackgroundColor3: "rgb(22,103,163)", clicked3: true }); if (this.state.clicked3) { this.setState({ buttonBackgroundColor3: "transparent", clicked3: false }); console.log(this.state.clicked3); } }}>
+                        <Text> 10-03-2019 </Text>
+                    </TouchableOpacity>
+                    </View>
+                </View>
+                </View>
+                
+            
+            
+        </ImageBackground>
+        </View>
+        
         </HeaderImageScrollView>
         
     );
@@ -145,11 +170,10 @@ class Council extends Component {
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      justifyContent: "space-between",
+      justifyContent: "flex-start",
     },
     questionContainer: {
-        flex: 1,
-        justifyContent: "center" 
+        //justifyContent: "center" 
     },
     answers: {
         flexDirection: "row",
@@ -174,8 +198,9 @@ const styles = StyleSheet.create({
     
     tabNav: {
         //marginLeft: -100,
+        //flex: 1,
+        //justifyContent: "flex-start",
         width: "100%",
-        flex: 1,
         height: 25,
         backgroundColor: "#373738",
         alignItems: "center",
