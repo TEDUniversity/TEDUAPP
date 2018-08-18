a7d8a5fc5fbd4b1b3f530154144a201c06da3d16
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-08-18 13:41:55 +0300

    Moodle forum page finished

---
+++ src/components/Moodle/Forum.tsx
@@ -0,0 +1,227 @@
+import React, { Component } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  ImageBackground,
+  ScrollView,
+  Dimensions,
+  Image
+} from "react-native";
+import {
+  StackNavigator,
+  createMaterialTopTabNavigator,
+  HeaderBackButton
+} from "react-navigation";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import Moodle from "../../screens/Moodle";
+import * as types from "../../store/types";
+import * as actions from "../../store/actions";
+import { connect } from "react-redux";
+import { Dispatch } from "redux";
+import { Spinner } from "../../screens/News";
+import { strip } from "../../util/helpers";
+
+interface IProp {
+  navigation: any;
+}
+interface ReduxProps {
+  user: types.User;
+}
+
+class Forum extends Component<IProp & ReduxProps> {
+  state = {
+    isLoading: true,
+    jsonToBeParsed: {},
+    id: ""
+  };
+
+  componentDidMount() {
+    this.getForums();
+  }
+
+  getPosts = () => {
+    var http = new XMLHttpRequest();
+    var url =
+      "https://moodle.tedu.edu.tr/webservice/rest/server.php?" +
+      "wstoken=" +
+      this.props.user.token +
+      "&moodlewsrestformat=json&wsfunction=mod_forum_get_forum_discussions_paginated" +
+      "&forumid=" +
+      this.state.id;
+    // alert(url);
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = () => {
+      //Call a function when the state changes.
+      if (http.readyState == 4 && http.status == 200) {
+        this.setState({
+          jsonToBeParsed: JSON.parse(http.response),
+          isLoading: false
+        });
+      }
+    };
+    http.send();
+  };
+
+  getForums = () => {
+    var http = new XMLHttpRequest();
+    var url =
+      "https://moodle.tedu.edu.tr/webservice/rest/server.php?" +
+      "wstoken=" +
+      this.props.user.token +
+      "&moodlewsrestformat=json&wsfunction=mod_forum_get_forums_by_courses" +
+      "&courseids[0]=" +
+      this.props.navigation.state.params.courseId;
+    http.open("POST", url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+    http.onreadystatechange = () => {
+      //Call a function when the state changes.
+      if (http.readyState == 4 && http.status == 200) {
+        this.setState({
+          jsonToBeParsed: JSON.parse(http.response),
+          id: JSON.parse(http.response)[0]["id"]
+        });
+        this.getPosts();
+      }
+    };
+    http.send();
+  };
+
+  renderElement = (elements: any) => {
+    return elements.map((data, Id) => {
+      let description = "";
+      if (data["description"]) {
+        description = strip(data["description"]);
+      }
+      if (data["visible"] === 1) {
+        return (
+          <View key={Id} style={{ flex: 1, flexDirection: "row" }}>
+            <Image
+              style={{ width: 20, height: 20, margin: 5 }}
+              source={{ uri: data["modicon"] }}
+            />
+            <View style={{ flex: 1, flexDirection: "column" }}>
+              <TouchableOpacity
+                onPress={() => {
+                  this.props.navigation.navigate("WebviewRouter", {
+                    url: data["url"],
+                    title: data["name"]
+                  });
+                }}
+              >
+                <Text
+                  style={{
+                    margin: 5,
+                    marginBottom: 0,
+                    fontSize: 13,
+                    color: "black"
+                  }}
+                >
+                  {data["name"]}
+                </Text>
+              </TouchableOpacity>
+              <Text
+                style={{
+                  fontSize: 10,
+                  color: "black"
+                }}
+              >
+                {description}
+              </Text>
+            </View>
+            {/* <Text style={{ fontSize: 15, margin: 5 }}>{data["summary"]}</Text> */}
+          </View>
+        );
+      } else {
+        return <View key={Id} />;
+      }
+    });
+  };
+
+  renderSection = (toBeMapped: any) => {
+    return toBeMapped.map((data, Id) => (
+      <View key={Id} style={styles.subContainer}>
+        <View
+          style={{
+            flex: 1,
+            flexDirection: "column",
+            justifyContent: "space-between"
+          }}
+        >
+          <Text style={{ fontSize: 20, fontWeight: "bold", margin: 5 }}>
+            {data["name"]}
+          </Text>
+          <Text style={{ fontSize: 17, fontStyle: "italic", margin: 5 }}>
+            {data["userfullname"]}
+          </Text>
+          <Text style={{ margin: 5 }}>{strip(data["message"])}</Text>
+        </View>
+      </View>
+    ));
+  };
+  render() {
+    if (this.state.isLoading) {
+      return <Spinner size={"large"} />;
+    } else {
+      return (
+        <ImageBackground
+          source={require("../../../img/background/BACKGROUND.png")}
+          style={styles.mainBackGround}
+        >
+          <ScrollView style={styles.container}>
+            {this.renderSection(this.state.jsonToBeParsed["discussions"])}
+          </ScrollView>
+        </ImageBackground>
+      );
+    }
+  }
+
+  static navigationOptions = {
+    title: "Detay",
+    headerStyle: { marginTop: 0, backgroundColor: "#fff" },
+    headerLeft: null,
+    gesturesEnabled: false
+  };
+}
+
+const styles = StyleSheet.create({
+  mainBackGround: {
+    flex: 1,
+    alignSelf: "stretch",
+    resizeMode: "cover"
+  },
+  container: {
+    flex: 1
+  },
+  text: {
+    fontWeight: "bold"
+  },
+  subContainer: {
+    flex: 1,
+    backgroundColor: "white",
+    width: Dimensions.get("window").width - 10,
+    // height: Dimensions.get("window").height / 6,
+    margin: 5
+  }
+});
+
+const mapStateToProps = (state: types.GlobalState) => {
+  return {
+    user: state.User
+  };
+};
+
+const mapDispatchToProps = (dispatch: Dispatch) => ({});
+
+export default connect<{}, {}, ReduxProps>(
+  mapStateToProps,
+  mapDispatchToProps
+)(Forum);
--- src/components/Moodle/MoodleDersDetay.tsx
+++ src/components/Moodle/MoodleDersDetay.tsx
@@ -21,49 +21,7 @@ import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import Detay from "./CourseContent";
 import Grades from "./Grades";
-
-interface IProp {
-  navigation: any;
-  courseId: string;
-}
-interface ReduxProps {
-  token: string;
-}
-
-class Forum extends Component<IProp> {
-  static navigationOptions = {
-    title: "Forum",
-    headerStyle: { marginTop: 0, backgroundColor: "#fff" },
-    headerLeft: null,
-    gesturesEnabled: false
-  };
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.subContainer}>
-          <Text style={styles.text}>This is ders detay.</Text>
-        </View>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "space-between",
-    alignItems: "center"
-  },
-  text: {
-    fontWeight: "bold"
-  },
-  subContainer: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center"
-  }
-});
+import Forum from "./Forum";
 
 const hScreen = createMaterialTopTabNavigator(
   {
