127defe2d3aaac713b03f4071a8aa38c49fdc050
Hayri Durmaz <hayri.durmaz@tedu.edu.tr>
2018-09-13 11:33:44 +0300

    webview file solved

--- src/components/Webview.tsx
+++ src/components/Webview.tsx
@@ -11,7 +11,8 @@ import {
   WebView,
   Alert,
   Platform,
-  BackHandler
+  BackHandler,
+  Linking
 } from "react-native";
 import Image from "react-native-scalable-image";
 import HeaderImageScrollView, {
@@ -19,6 +20,9 @@ import HeaderImageScrollView, {
 } from "react-native-image-header-scroll-view";
 import { Header } from "react-navigation";
 import Icon from "react-native-vector-icons/Ionicons";
+import RNFetchBlob from "rn-fetch-blob";
+import FileViewer from "react-native-file-viewer";
+
 const Spinner = ({ size }) => <ActivityIndicator size={size || "large"} />;
 
 interface IProp {
@@ -38,7 +42,7 @@ class Webview extends React.Component<IProp> {
       </Text>
     ),
 
-    title: "Webview",
+    title: "Web",
     headerStyle: {
       marginTop: 0,
       backgroundColor: "#144d8c",
@@ -116,6 +120,17 @@ class Webview extends React.Component<IProp> {
     }
   }
 
+  openExternalLink(req) {
+    const isLocal = req.url.search("http://localhost") !== -1;
+
+    if (isLocal) {
+      return true;
+    } else {
+      Linking.openURL(req.url);
+      return false;
+    }
+  }
+
   render() {
     console.log(this.props.navigation.state.params.url);
     return (
@@ -126,7 +141,7 @@ class Webview extends React.Component<IProp> {
           }}
           source={{ uri: this.props.navigation.state.params.url }}
           style={{ marginTop: 0 }}
-          startInLoadingState={true}
+          //   startInLoadingState={true}
           renderLoading={() => {
             return <Spinner size={"large"} />;
           }}
@@ -134,8 +149,49 @@ class Webview extends React.Component<IProp> {
             this.loadError();
           }}
           onNavigationStateChange={navState => {
-            this.webView.canGoBack = navState.canGoBack;
+            // this.webView.canGoBack = navState.canGoBack;
+            if (
+              navState.url.includes(".pdf") ||
+              navState.url.includes(".xlsx") ||
+              navState.url.includes(".docx")
+            ) {
+              //   this.webView.ref.stopLoading();
+              //   Linking.openURL(navState.url);
+              let dirs = RNFetchBlob.fs.dirs;
+              RNFetchBlob.config({
+                // add this option that makes response data to be stored as a file,
+                // this is much more performant.
+                fileCache: true,
+                path: dirs.DocumentDir + "/" + navState.url.replace(/ /g, "")
+              })
+                .fetch("GET", navState.url, {
+                  //some headers ..
+                })
+                .then(res => {
+                  // the temp file path
+                  this.setState({ fileLoading: false });
+                  console.log("The file saved to ", res.path());
+                  FileViewer.open(res.path())
+                    .then(() => {
+                      console.log("success");
+                    })
+                    .catch(error => {
+                      console.log(error);
+                      Alert.alert(
+                        "Hata",
+                        "Bir hata oluştu: lütfen bir office programı indirin!"
+                      );
+                    });
+                })
+                .catch(err => {
+                  console.error(err);
+                });
+            } else {
+              this.webView.canGoBack = navState.canGoBack;
+            }
           }}
+          javaScriptEnabled={true}
+          onShouldStartLoadWithRequest={this.openExternalLink}
 
           //onNavigationStateChange={this.onNavigationStateChange }
         />
@@ -145,7 +201,7 @@ class Webview extends React.Component<IProp> {
             flexDirection: "row",
             justifyContent: "space-between",
             alignItems: "center",
-            height: 30,
+            height: 45,
             backgroundColor: "rgb(24, 79, 138)"
           }}
         >
